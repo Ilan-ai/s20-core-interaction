c446d868dfe7fadb05e47bf018ba1d4a11a4d661
Ilan-ai <ibelokon123@gmail.com>
2020-01-30 23:43:31 -0500

    update

---
+++ js/home.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  $("#circle1").on("click", function(){
+    $("footer").css("visibility","hidden")
+    $("#about_text").css("visibility","hidden")
+
+});
+
+$("#triangle3").on("click", function(){
+  $("#about_text").css("visibility","visible")
+  $("footer").css("visibility","hidden")
+});
+
+$("#square2").on("click", function(){
+  $("footer").css("visibility","visible")
+  $("#about_text").css("visibility","hidden")
+});
+
+
+})
