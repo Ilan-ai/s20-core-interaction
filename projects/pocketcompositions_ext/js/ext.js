635456913688832f1b28e38416014d7ffe29824d
Ilan-ai <ibelokon123@gmail.com>
2020-02-27 02:59:19 -0500

    update

--- projects/pocketcompositions_ext/js/ext.js
+++ projects/pocketcompositions_ext/js/ext.js
@@ -2,23 +2,40 @@ var s1 = document.getElementById("square1");
 
 setInterval(function(){
 
-for (var i = 0; i < 50; i++) {
+for (var i = 0; i < 5; i++) {
 
-    var a1 = Math.round(Math.random()*255); // 144.77989
-    var a2 = Math.round(Math.random()*255);
+  if( i < 5){
+
+
+  var color1 = Math.floor(Math.random()*16777215).toString(16);
+  var color2 = Math.floor(Math.random()*16777215).toString(16);
+// i need two seperate colors generated and then plug those colors into the "Mymodule" vairable :(
+
+
+
+
+    var wholeset= document.getElementById("square");
 
     var firstVariable = 'replace me with some css';
 
     var myModule = `
 
         <div id="square1"
-        style="background-image: linear-gradient(to right ${a1}, ${a2});">
+        style="background-image: linear-gradient(to right, #${color1}, #${color2});">
+        </div>
+        <div id="square2"
+        style="background-image: linear-gradient(to right, #${color1}, #${color2});">
+        </div>
+        <div id="square3"
+        style="background-image: linear-gradient(to right, #${color1}, #${color2});">
         </div>
 
     `;
 
-    myRootDiv.insertAdjacentHTML('beforeend', myModule)
+    wholeset.insertAdjacentHTML('beforeend', myModule);
 
-
+    i= i + 1
+    console.log(i)
+}
 }
 }, 1000)
