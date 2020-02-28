b43f6727f38dff783ec40ab8fd5b1d851f482071
Ilan-ai <ibelokon123@gmail.com>
2020-02-27 23:28:10 -0500

    updates

--- projects/pocketcompositions_ext/js/ext.js
+++ projects/pocketcompositions_ext/js/ext.js
@@ -1,22 +1,14 @@
 var s1 = document.getElementById("square1");
 
-setInterval(function(){
-
-for (var i = 0; i < 5; i++) {
-
-  if( i < 5){
-
+for (var i = 0; i < 50; i++) {
 
   var color1 = Math.floor(Math.random()*16777215).toString(16);
   var color2 = Math.floor(Math.random()*16777215).toString(16);
 // i need two seperate colors generated and then plug those colors into the "Mymodule" vairable :(
 
-
-
-
     var wholeset= document.getElementById("square");
 
-    var firstVariable = 'replace me with some css';
+  //  var firstVariable = 'replace me with some css';
 
     var myModule = `
 
@@ -34,8 +26,6 @@ for (var i = 0; i < 5; i++) {
 
     wholeset.insertAdjacentHTML('beforeend', myModule);
 
-    i= i + 1
     console.log(i)
+
 }
-}
-}, 1000)
